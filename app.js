1e10aa315acc181fc2a810815821ae11af4209b5
Gible147 <68395812+Gible147@users.noreply.github.com>
2022-06-08 13:44:04 -0400

    Tim (#1)
    * set up nodejs backend server, basic ejs, basic homepage
    
    * fixed favico
    
    * added some stuff to home page
    
    Co-authored-by: timrolsh <tim.rolsh@gmail.com>

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const logger = require("morgan");
+const favicon = require('serve-favicon');
+
+// get morgan logger to write to console
+app.use(logger("dev"));
+
+// public folder will have all javascripts htmls and css that our page will render
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/public/html");
+app.set("view engine", "ejs");
+
+// make express work with post requests: make sure to JSON.stringify the bodies of all post requests from frontend, sending just javascript objects will result in empty body
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// put icon in the browser
+app.use(favicon(__dirname + "/public/images/browserIcon.ico"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// start server and have it listen to the part
+app.listen(port, () => {
+    console.log(
+        `App server listening on ${port}. URL: http://localhost:${port})`
+    );
+});
