5edc46863ecabfca58207d752675ceb6c4ab20fc
davidlee06 <davidshinlee13@gmail.com>
2022-06-12 22:30:19 -0400

    formatted some stuff

--- public/javascripts/city.js
+++ public/javascripts/city.js
@@ -11,14 +11,19 @@ function flipUnits() {
     let tableCols = [];
     for (let i = 0; i < tableRows; ++i) {
         for (let j = 0; j < tableRows.cells.length; ++j) {
-            tableCols.push(tableRows.cells[j])
+            tableCols.push(tableRows.cells[j]);
         }
     }
 
     if (document.getElementById("tempUnits").innerHTML.substring(1) === "C") {
         for (let i = 0; i < tableCols.length; ++i) {
-            let tempTemp = parseInt(tableCols[i].innerHTML.substring(0, tableCols[i].innerHTML.length - 2));
-            tableCols[i].innerHTML = parseInt(tempTemp * 9)
+            let tempTemp = parseInt(
+                tableCols[i].innerHTML.substring(
+                    0,
+                    tableCols[i].innerHTML.length - 2
+                )
+            );
+            tableCols[i].innerHTML = parseInt(tempTemp * 9);
         }
     }
     // otherwise, flip everything to C
