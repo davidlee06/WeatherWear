20ec2723fa7731e3cb98b94a609e42d835af992f
timrolsh <68395794+timrolsh@users.noreply.github.com>
2022-06-12 22:40:15 -0400

    weather api and city page works
    * made new branch
    
    * fetch does not work
    
    * api works ffs
    
    * fixed api
    
    * put data on city page
    
    * started working on units button flipper
    
    * formatted some stuff
    
    Co-authored-by: jaydenmaeng <jmaeng06@gmail.com>
    Co-authored-by: davidlee06 <davidshinlee13@gmail.com>

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const logger = require("morgan");
+const dotenv = require("dotenv");
+const axios = require("axios");
+dotenv.config();
+const apiKey = process.env.apiKey;
 
 // get morgan logger to write to console
 app.use(logger("dev"));
@@ -20,7 +24,60 @@ app.get("/", (req, res) => {
 });
 
 app.post("/city", (req, res) => {
-    res.render("city", { info: {cityName: req.body.cityName} });
+    let stringArray = req.body.cityName.split(", ");
+    if (stringArray.length < 3) {
+        res.statusCode = 404;
+        res.send("invalid city type specified");
+    }
+    axios
+        .get(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${stringArray[0]},${stringArray[1]},${stringArray[2]}&limit=1&appid=${apiKey}`
+        )
+        .then((response) => {
+            response.data[0];
+            axios
+                .get(
+                    `http://api.openweathermap.org/data/2.5/forecast?lat=${response.data[0].lat}&lon=${response.data[0].lon}&appid=${apiKey}`
+                )
+                .then((otherResponse) => {
+                    otherResponse = otherResponse.data;
+                    let newObject = { cityName: req.body.cityName, array: [] };
+                    for (let i = 0; i < otherResponse.list.length; ) {
+                        let tempTemps = [];
+                        let hoursPassed = 0;
+                        let tempDateString = new Date(
+                            otherResponse.list[i].dt * 1000
+                        ).toDateString();
+                        hoursPassed = new Date(
+                            otherResponse.list[i].dt * 1000
+                        ).getHours();
+                        while (
+                            hoursPassed < 24 &&
+                            i < otherResponse.list.length
+                        ) {
+                            tempTemps.push(otherResponse.list[i].main.temp);
+                            ++i;
+                            hoursPassed += 3;
+                        }
+                        // sort array, first one will be the smallest temp, other one will be the largest temp
+                        tempTemps.sort((a, b) => {
+                            if (a < b) {
+                                return -1;
+                            }
+                            if (a > b) {
+                                return 1;
+                            }
+                            return 0;
+                        });
+                        newObject.array.push([
+                            tempDateString,
+                            tempTemps[0],
+                            tempTemps[tempTemps.length - 1],
+                        ]);
+                    }
+                    res.render("city", newObject);
+                });
+        });
 });
 
 app.get("/outfit", (req, res) => {
---
+++ public/javascripts/city.js
@@ -0,0 +1,32 @@
+function redirect() {
+    window.location.replace("outfit");
+}
+function goBack() {
+    window.location.replace("/");
+}
+
+function flipUnits() {
+    // if the units are C flip everything to F
+    let tableRows = document.getElementsByClassName("tableRow");
+    let tableCols = [];
+    for (let i = 0; i < tableRows; ++i) {
+        for (let j = 0; j < tableRows.cells.length; ++j) {
+            tableCols.push(tableRows.cells[j]);
+        }
+    }
+
+    if (document.getElementById("tempUnits").innerHTML.substring(1) === "C") {
+        for (let i = 0; i < tableCols.length; ++i) {
+            let tempTemp = parseInt(
+                tableCols[i].innerHTML.substring(
+                    0,
+                    tableCols[i].innerHTML.length - 2
+                )
+            );
+            tableCols[i].innerHTML = parseInt(tempTemp * 9);
+        }
+    }
+    // otherwise, flip everything to C
+    else {
+    }
+}
