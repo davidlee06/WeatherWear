715d029b504c6b21be7e0624580c4782a3d6cabc
timrolsh <tim.rolsh@gmail.com>
2022-07-26 16:24:56 -0400

    cleaned up homepage

--- app.ts
+++ app.ts
@@ -1,23 +1,23 @@
 import morgan from "morgan";
-import axios from "axios";
 import fs from "fs";
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
-
+import path from "path";
+const rootPath: string = path.resolve(__dirname, "..");
 // if debug is set to true, use morgan in dev mode
 if (process.env.DEBUG) {
     // get morgan logger to write to console
     app.use(morgan("dev"));
 }
 // server bootstrap files from our local node_modules instead of cdn
-app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap`));
+app.use("/bootstrap", express.static(`${rootPath}/node_modules/bootstrap`));
 
-// set up routes for statuc files
-app.use(express.static(__dirname + "/static"));
+// set up routes for static files
+app.use(express.static(`${rootPath}/static`));
 // set up server side rendering with ejs
-app.set("views", __dirname + "/ssr");
+app.set("views", `${rootPath}/ssr`);
 app.set("view engine", "ejs");
 
 // make express work with json
@@ -27,7 +27,7 @@ app.use(express.json());
 // if ./static/images/generatedOutfits exists, delete all the files in it
 // otherwise, make the directory
 if (fs.existsSync("./static/images/generatedOutfits")) {
-    console.log("clearing generatedOutfits directory...");
+    console.log("Found generatedOutfits directory, clearing ...");
     const files = fs.readdirSync("static/images/generatedOutfits");
     for (let file in files) {
         fs.unlinkSync(`static/images/generatedOutfits/${files[file]}`);
@@ -37,3 +37,7 @@ if (fs.existsSync("./static/images/generatedOutfits")) {
     fs.mkdirSync("./static/images/generatedOutfits");
 }
 
+// start server and have it listen to the part
+app.listen(process.env.PORT, () => {
+    console.log(`Server launched on port ${process.env.PORT}. URL: http://localhost:${process.env.PORT})`);
+});
