5a8040f44fc6721d336b2761095ee95f13a55324
timrolsh <tim.rolsh@gmail.com>
2022-07-29 14:22:23 -0400

    fixed db site is broken

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,22 +1,32 @@
-create schema weatherwear;
+create schema "weatherwear";
 
-create table weatherwear."user"
-(
-    id              text  not null,
-    user_name       text  not null,
+create table weatherwear.outfit_piece (
+    id serial primary key,
+    x integer not null,
+    y integer not null,
+    data bytea not null,
+    body_part text not null,
+    temp_range text not null
+);
+
+create table weatherwear."user" (
+    id text not null primary key,
+    user_name text not null,
     profile_picture bytea not null
 );
 
-create table weatherwear.outfit
-(
-    user_id      text    not null,
-    outfit_image integer not null,
-    date_created integer not null
+create table weatherwear.location (
+    id integer not null primary key,
+    city text not null,
+    state_code text not null,
+    user_id text not null,
+    constraint fk_user_id foreign key (user_id) references weatherwear."user" (id)
 );
 
-create table weatherwear.location
-(
-    city       integer not null,
-    state_code integer not null,
-    user_id    text    not null
+create table weatherwear.outfit (
+    id integer not null primary key,
+    user_id text not null,
+    data bytea not null,
+    time_generated integer not null,
+    constraint fk_user_id foreign key (user_id) references weatherwear."user" (id)
 );
--- server/LaunchServer.ts
+++ server/LaunchServer.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import server from "./ExpressConfig";
-require("./ServerRoutes")
+require("./ServerRoutes");
 // start server and have it listen to the part
 server.listen(process.env.PORT, () => {
     console.log(`Server launched on port ${process.env.PORT}. URL: http://localhost:${process.env.PORT})`);
