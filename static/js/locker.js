a3bb36cdb77b2ca6782618b21c3d70fefc00a393
RohanJoshi28 <78099546+RohanJoshi28@users.noreply.github.com>
2022-10-30 18:11:18 -0400

    add locker styling files, fix scrolling bug

--- static/js/locker.js
+++ static/js/locker.js
@@ -6,9 +6,9 @@ const images_root = document.getElementById("images_root");
 fetch("/api/locker-image-info", {credentials: "same-origin"}).then((fetchResponse) => {
     fetchResponse.json().then((array) => {
         for (let a = 0; a < array.length; ++a) {
-            images_root.innerHTML += `<div id="image-${a}"><div>Outfit #${a + 1}: Time created: ${
+            images_root.innerHTML += `<div id="image-${a}"><h3 style="margin-top: 10px; margin-bottom: 10px;">Outfit #${a + 1}: Time created: ${
                 array[a]["time_created"]
-            }</div><img src = "/api/get-image?image_id=${array[a]["id"]}"/><button class = "remove_button"
+            }</h3><img src = "/api/get-image?image_id=${array[a]["id"]}"/><button class = "btn btn-primary remove_button"
             }">Remove this outfit from locker</button></div>`;
         }
         const remove_buttons = document.getElementsByClassName("remove_button");
